78660c832df788cde63142238b888d95687e9fec
Tarik8 <tariktekelioglu8@gmail.com>
2018-04-18 21:59:16 +0100

    Completed the questions and answers files.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,8 +2,17 @@ package Main;
 
 import Quiz.ComputerScience;
 
-/* As of now (writing the code), I don't have a lot of experience with file writing and reading, which is why my folder
- organization method is a mess. */
+/*
+- As of now (writing the code), I don't have a lot of experience with file writing and reading, which is why my folder
+ organization method is a mess.
+
+- I actually took the time to write all the questions and answers (computer science questions fully written by me (as in
+I didn't use Google to clarify anything..
+
+- If this coursework didn't get leaked and actually counted, I would've made this program a lot more better by building
+a graphical user interface, and actually cleaning up the code..*/
+
+
 
 public class Main {
 
