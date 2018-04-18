bf82626f1729ebc83375e3ce8a9c33459d61bd42
Tarik8 <tariktekelioglu8@gmail.com>
2018-04-18 22:25:48 +0100

    Created the answers.

--- src/Quiz/ComputerScience.java
+++ src/Quiz/ComputerScience.java
@@ -17,17 +17,33 @@ public class ComputerScience extends MainQuiz {
 
     } // Tried, doesn't work */
 
-    static File temp = new File("src/Quiz/Questions/Computer Science.txt");
+    public static int score;
 
 
     public static void Easy() throws IOException {
 
-        Scanner file = new Scanner(temp);
+        boolean validation = true;
 
-        for (int x = 0; x < 6; x++) {
-            System.out.println(file.nextLine());
-        }
+        File q1txt = new File("src/Quiz/Questions/Easy/Computer Science/Q1.txt");
+        File q2txt = new File("src/Quiz/Questions/Easy/Computer Science/Q2.txt");
 
+        Scanner q1 = new Scanner(q1txt);
+        String input;
+
+        do {
+            Scanner in = new Scanner(System.in);
+
+            while (q1.hasNext()) {
+                System.out.println(q1);
+            }
+
+            input = in.next();
+
+            if (input.equalsIgnoreCase("a")) {
+
+            }
+
+        } while (validation == true);
     }
 
     public static void Medium() {
