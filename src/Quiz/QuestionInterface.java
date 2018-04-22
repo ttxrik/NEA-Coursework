b41aa0991c6a6457d4e0273e420dde864c5245eb
Tarik8 <tariktekelioglu8@gmail.com>
2018-04-22 20:48:54 +0100

    Updated

---
+++ src/Quiz/QuestionInterface.java
@@ -0,0 +1,26 @@
+package Quiz;
+
+public abstract class QuestionInterface extends MainQuiz{
+
+    void Q1(){}
+
+    void Q2(){}
+
+    void Q3(){}
+
+    void Q4(){}
+
+    void A1(){}
+
+    void A2(){}
+
+    void A3(){}
+
+    void A4(){}
+
+    void Easy(){}
+
+    void Medium(){}
+
+    void Hard(){}
+}
